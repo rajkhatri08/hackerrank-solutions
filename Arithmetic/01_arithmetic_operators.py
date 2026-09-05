2fa68d713bbf725529a05d5c65a6214e7b78616f
Raj Khatri <rajkhatrigbpuat@gmail.com>
2026-09-05 22:17:27 +0530

    feat: solve arithmetic operators challenge

---
+++ Arithmetic/01_arithmetic_operators.py
@@ -0,0 +1,16 @@
+#The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+#The first line contains the sum of the two numbers.
+#The second line contains the difference of the two numbers (first - second).
+#The third line contains the product of the two numbers.
+
+#so here the first line wants sum of a,b
+# second line wants subtraction of a,b
+#third line wants multiplication of a and b
+
+a = int(input())
+b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
+
+
